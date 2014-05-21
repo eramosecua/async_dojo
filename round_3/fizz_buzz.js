f76bc0130de4b40b539b287a2db44e8a46fcbfdc
edgar <eramosecua@gmail.com>
2014-05-21 07:12:11 -0500

    added q library for promise

--- round_3/fizz_buzz.js
+++ round_3/fizz_buzz.js
@@ -3,35 +3,54 @@
 var fs = require('fs');
 var map = require('../round_1/fizz_buzz.js');
 var _ = require('underscore');
+var Q = require('q');
 
 function main_async (callback){
+  var deferred = Q.defer();
   var results = [];
 
-  get_list_files_async(function (files) {
+  return get_list_files_async()
+  .then(function (files) {
     _(files).each(function (file) {
       var filepath = __dirname + '/input_files/'+file;
 
       fs.readFile(filepath, 'utf8', function (error, lines) {
+
+        if (error) {
+          deferred.reject(error);
+        }
+
         var data = JSON.parse(lines);
         results.push(map.execute(data));
-        if(results.length == files.length) {
-          callback(error, results);
+
+        if(results.length === files.length) {
+          deferred.resolve(results);
+          console.log('aui...')
         }
       });
+      return deferred.promise;
     });
   });
 }
 
 function get_list_files_async (callback) {
+  var deferred = Q.defer();
   var filepath = __dirname + '/input_files/files_to_read';
+
   fs.readFile(filepath, 'utf8', function (error, lines) {
+    if(error) {
+      deferred.reject(error);
+    }
+
     var data = lines.split( "\n" );
-    callback(data);
+    deferred.resolve(data);
   });
+
+  return deferred.promise;
 }
 
 module.exports = {
   execute_async: function (callback) {
-    main_async(callback);
+    return  main_async(callback);
   }
 };
