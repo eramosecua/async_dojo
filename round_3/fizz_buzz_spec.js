8b51f464ae26845bb14d18f4b8aa41258ec76db8
edgar <eramosecua@gmail.com>
2014-05-21 06:14:42 -0500

    added files for round_3

---
+++ round_3/fizz_buzz_spec.js
@@ -0,0 +1,26 @@
+"use strict";
+
+describe('kata fizz buzz round_2', function () {
+  var api;
+  var _ = require('underscore');
+
+  beforeEach(function () {
+    api = require('./fizz_buzz.js');
+  });
+
+  it('should  ', function (done) {
+    api.execute_async(function (error, data) {
+      expect(data.length).toBe(3);
+      expect(data).toContain([1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz']);
+      expect(data).toContain(['buzz', 'fizz', 22, 23, 'fizz', 'buzz', 26, 'fizz', 28, 'Luinix_for_president', 'fizzbuzz']);
+      expect(data).toContain([ 1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz' ] );
+
+      if(error) {
+        done('error inesperado');
+      }
+
+      done();
+    });
+
+  });
+});
