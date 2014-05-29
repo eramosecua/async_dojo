309817e7cefeeb0ac117174c5777ac490d8d8c9e
Edgar Ramos <edgar.ramos@provedatos.com>
2014-05-29 17:37:37 -0500

    refactor fizz buzz round1

--- round_1/fizz_buzz.js
+++ round_1/fizz_buzz.js
@@ -3,24 +3,15 @@
 var _ = require('underscore');
 
 function fizz(number) {
-  if (number % 3 === 0 && number % 5 !== 0) {
-    return true;
-  }
-  return false;
+  return (number % 3 === 0 && number % 5 !== 0);
 }
 
 function buzz (number) {
-  if(number % 5 === 0 && number % 3 !== 0) {
-    return true;
-  }
-  return false;
+  return (number % 5 === 0 && number % 3 !== 0);
 }
 
 function fizzbuzz(number) {
-  if(number % 5 === 0 && number % 3 === 0) {
-    return true;
-  }
-  return false;
+  return (number % 5 === 0 && number % 3 === 0);
 }
 
 module.exports = {
